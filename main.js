485c05fd012ca6c0a6c6fefaba1881314ce83afa
Code-Hello <m.tuto@protonmail.com>
2020-03-26 10:41:56 +0100

    exercise solved

---
+++ main.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+
+const log = console.log;
+log(chalk.blueBright('Ameline') + ' ' + chalk.red('Nicolas') + ' ' + chalk.greenBright('Brice') + ' ' + chalk.magenta('Alice'));
